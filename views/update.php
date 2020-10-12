3753e575fb2bea68819fa3eda1c2cf55bab3b1f4
jubis18 <edgars.juberts@gmail.com>
2020-10-12 23:15:12 +0200

    style integration

--- views/update.php
+++ views/update.php
@@ -4,11 +4,36 @@ include $_SERVER['DOCUMENT_ROOT'].'/layout/header.php';
 include $_SERVER['DOCUMENT_ROOT']."/db/connector.php";
 include $_SERVER['DOCUMENT_ROOT']."/models/base.php";
 
-    echo " </br> Update project task: ". $_GET['update_task'];
-    $task_id = $_GET['update_task'];
-    $result =Base::projectInfoById($task_id);
-    
+echo " </br> Update project task: ". $_GET['update_task'];
+$task_id = $_GET['update_task'];
+$result =Base::projectInfoById($task_id);
 
+if(isset($_POST['submit'])) {
+        
+    ?>
+    
+    <div class="alert alert-success" role="alert">
+    You have updated the task !!
+    </div>
+
+<?php
+
+$task_id = $_POST['taskid'];
+$task = $_POST['task'];
+$time = $_POST['time'];
+$date = $_POST['date'];
+
+
+print_r("<form action='' method='post' >
+<input type='hidden' name = 'taskid' value ='$task_id'>
+<input type='text' name = 'task' value='$task'>
+<input type = 'time' name ='time' value = '$time'>
+<input type = 'date' name = 'date' value = '$date' >
+<input type = 'submit' name='submit' value = 'update'>
+    </form>");
+
+} else {
+    
     while($row = $result->fetchArray() ) {
         $task = $row['project_task'];
         $time = $row['time'];
@@ -23,6 +48,8 @@ include $_SERVER['DOCUMENT_ROOT']."/models/base.php";
         
         
     }
+}
+
 
     if(isset($_POST['time'], $_POST['date'], $_POST['task'] ) ){
         $id = $_POST['taskid'];
@@ -30,12 +57,6 @@ include $_SERVER['DOCUMENT_ROOT']."/models/base.php";
         $date = $_POST['date'];
         $task = $_POST['task'];
         Base::update_task($id, $time, $date, $task);
-    }
-
-    if(isset($_POST['submit'])) {
-        
-        $id =$_POST['taskid'];
-        header("Location: /index.php");
     }
 
 
